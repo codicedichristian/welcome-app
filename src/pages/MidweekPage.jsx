5e5bc2bee5b6ec1c957c276a9a3b0427dbb82af7
Christian Scorza <chrix94x2@gmail.com>
2026-09-09 16:25:56 +0200

    fix: update CARTO tile URL to new endpoint (old fastly CDN deprecated)

--- src/pages/MidweekPage.jsx
+++ src/pages/MidweekPage.jsx
@@ -40,7 +40,7 @@ function MapFlyTo({ group }) {
   return null
 }
 
-const TILE_URL = 'https://cartodb-basemaps-a.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png'
+const TILE_URL = 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
 const midweekEvent = normalizeEvent(getEventById('midweek'))
 
 function createPinIcon(selected) {
